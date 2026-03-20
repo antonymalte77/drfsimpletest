7bada452d03d88b27e609460f4c29e6631233178
antonymalte77 <antonymalte77@gmail.com>
2026-03-20 00:50:07 -0500

    Corregido nombre de ProjectSerializer para coincidir con la vista

--- projects/serializers.py
+++ projects/serializers.py
@@ -1,8 +1,8 @@
 from rest_framework import serializers
-from .models import project
+from .models import project # Asegúrate de que en models.py la clase sea 'project'
 
-class ProjectSerializer(serializers.ModelSerializer):
+class ProjectSerializer(serializers.ModelSerializer): # <--- DEBE LLAMARSE ASÍ
     class Meta:
         model = project
-        fields = ('id', 'titulo', 'descripcion', 'tecnologia', 'f_creacion')  
+        fields = ('id', 'titulo', 'descripcion', 'tecnologia', 'f_creacion')   
         read_only_fields = ('f_creacion', )
