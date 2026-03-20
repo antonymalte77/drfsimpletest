75dd81d4446007778be6b477e43f739b62cf9d61
antonymalte77 <antonymalte77@gmail.com>
2026-03-20 14:16:23 -0500

    API completa con estilos y permisos CORS

--- drfsimplecrud/settings.py
+++ drfsimplecrud/settings.py
@@ -136,3 +136,6 @@ if not DEBUG:
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Permitir que el navegador acepte respuestas de cualquier origen (CORS)
+CORS_ALLOW_ALL_ORIGINS = True
